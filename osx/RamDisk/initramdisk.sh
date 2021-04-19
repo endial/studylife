d5d9acb59fb213d9554e46d4ad98efce2a759c90
Endial Fang <endial@126.com>
2021-04-19 15:10:03 +0800

    [osx]RamDisk脚本增加初始化Documents目录

--- osx/RamDisk/initramdisk.sh
+++ osx/RamDisk/initramdisk.sh
@@ -7,7 +7,7 @@ MOUNT_PATH=/Volumes/$DISK_NAME
 WORK_PATH=/etc/RamDisk
 BAK_PATH=$WORK_PATH/$DISK_NAME.tar.gz
 # 设置分配给内存盘的空间大小(MB)
-DISK_SPACE=4000
+DISK_SPACE=4096
 
 # 创建Ramdisk
 if [ ! -e $MOUNT_PATH ]; then
@@ -16,10 +16,12 @@ fi
 
 dev=`hdid -nomount ram://$(($DISK_SPACE*1024*2))`
 newfs_hfs -v $DISK_NAME $dev
-mount -o noatime,nosuid,-w,-m=777 -t hfs $dev $MOUNT_PATH    
+mount -o noatime,nosuid,-w,-m=755 -t hfs $dev $MOUNT_PATH    
 chown endial:staff $MOUNT_PATH
 
 # 恢复备份
 if [ -s $BAK_PATH ]; then
     tar -zxf $BAK_PATH -C $MOUNT_PATH
+else
+    mkdir -p $MOUNT_PATH/{Caches,Containers,Documents,Downloads,Temp,Volumes}
 fi
